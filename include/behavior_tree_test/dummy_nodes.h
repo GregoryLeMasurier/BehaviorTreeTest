a21dd004ba4f43772d23445744e12294f274e3b0
GregoryLeMasurier <gregorylemasurier@gmail.com>
2021-08-23 16:17:32 -0400

    remove unused parts

--- include/behavior_tree_test/dummy_nodes.h
+++ include/behavior_tree_test/dummy_nodes.h
@@ -13,60 +13,6 @@ namespace DummyNodes
 
 BT::NodeStatus CheckBattery();
 
-class GripperInterface
-{
-  public:
-    GripperInterface() : _opened(true)
-    {
-    }
-
-    BT::NodeStatus open();
-
-    BT::NodeStatus close();
-
-  private:
-    bool _opened;
-};
-
-//--------------------------------------
-
-// Example of custom SyncActionNode (synchronous action)
-// without ports.
-class ApproachObject : public BT::CoroActionNode
-{
-  public:
-    ApproachObject(const std::string& name) :
-        BT::CoroActionNode(name, {})
-    {
-    }
-
-    // You must override the virtual function tick()
-    BT::NodeStatus tick() override;
-};
-
-// Example of custom SyncActionNode (synchronous action)
-// with an input port.
-class SaySomething : public BT::CoroActionNode
-{
-  public:
-    SaySomething(const std::string& name, const BT::NodeConfiguration& config)
-      : BT::CoroActionNode(name, config)
-    {
-    }
-
-    // You must override the virtual function tick()
-    BT::NodeStatus tick() override;
-
-    // It is mandatory to define this static method.
-    static BT::PortsList providedPorts()
-    {
-        return{ BT::InputPort<std::string>("message") };
-    }
-};
-
-//Same as class SaySomething, but to be registered with SimpleActionNode
-BT::NodeStatus SaySomethingSimple(BT::TreeNode& self);
-
 class MyAsyncAction: public CoroActionNode
 {
   public:
@@ -146,70 +92,9 @@ class MyAsyncAction: public CoroActionNode
     }
 };
 
-// Action_A has a different constructor than the default one.
-class Action_A: public SyncActionNode
-{
-
-public:
-    // additional arguments passed to the constructor
-    Action_A(const std::string& name, const NodeConfiguration& config,
-             int arg1, double arg2, std::string arg3 ):
-        SyncActionNode(name, config),
-        _arg1(arg1),
-        _arg2(arg2),
-        _arg3(arg3) {}
-
-    NodeStatus tick() override
-    {
-        std::cout << "Action_A: " << _arg1 << " / " << _arg2 << " / " << _arg3 << std::endl;
-        return NodeStatus::SUCCESS;
-    }
-    static PortsList providedPorts() { return {}; }
-
-private:
-    int _arg1;
-    double _arg2;
-    std::string _arg3;
-};
-
-// Action_B implements an init(...) method that must be called once at the beginning.
-class Action_B: public SyncActionNode
-{
-
-public:
-    Action_B(const std::string& name, const NodeConfiguration& config):
-        SyncActionNode(name, config) {}
-
-    // we want this method to be called ONCE and BEFORE the first tick()
-    void init( int arg1, double arg2, std::string arg3 )
-    {
-        _arg1 = (arg1);
-        _arg2 = (arg2);
-        _arg3 = (arg3);
-    }
-
-    NodeStatus tick() override
-    {
-        std::cout << "Action_B: " << _arg1 << " / " << _arg2 << " / " << _arg3 << std::endl;
-        return NodeStatus::FAILURE;
-    }
-    static PortsList providedPorts() { return {}; }
-
-private:
-    int _arg1;
-    double _arg2;
-    std::string _arg3;
-};
-
 inline void RegisterNodes(BT::BehaviorTreeFactory& factory)
 {
-    static GripperInterface grip_singleton;
-
     factory.registerSimpleCondition("CheckBattery", std::bind(CheckBattery));
-    factory.registerSimpleAction("OpenGripper", std::bind(&GripperInterface::open, &grip_singleton));
-    factory.registerSimpleAction("CloseGripper", std::bind(&GripperInterface::close, &grip_singleton));
-    factory.registerNodeType<ApproachObject>("ApproachObject");
-    factory.registerNodeType<SaySomething>("SaySomething");
     factory.registerNodeType<MyAsyncAction>("MyAsyncAction");
 }
 
